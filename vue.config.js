465147374729b7e75925b383651c61b6ad9b8555
Qinx <qinx@smartsalary.cn>
2020-08-06 14:50:51 +0800

    not build debounce in bundle

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-fold-slide/dist/'
-    : '/'
+    : '/',
+
+  // css: { extract: false },
+  configureWebpack: {
+    externals: {
+      debounce: 'debounce'
+    }
+  }
 }
